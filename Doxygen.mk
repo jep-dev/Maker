791ed7d3df791817e0bd61839ecddb2ee4a06abb
John Petersen <sobeita@gmail.com>
2017-01-20 17:45:00 -0500

    Added sentinel for doxygen output

--- Doxygen.mk
+++ Doxygen.mk
@@ -8,19 +8,20 @@ doc_dir?=doc/
 doc_name?=Doxyfile
 doc_ext?=
 doc_file?=$(doc_dir)$(doc_name)$(doc_ext)
-#doc_pre_file?=$(doc_dir).pre$(sentinel_ext)
-#doc_post_file?=$(doc_dir).post$(sentinel_ext)
-#doc_files?=$(doc_pre_file) $(doc_file) $(doc_post_file)
-#doc_files?=$(foreach F,_name _pre_name _post_name,$(doc_dir)$(doc_$(F)))
-doc_files?=$(doc_file)
+doc_sentinel?=$(doc_dir).$(doc_name)$(sentinel_ext)
+doc_files?=$(doc_sentinel) $(doc_file)
 
 # To define <X> in your Doxyfile, define DOXY_<X> in your makefile
 # For example, DOXY_PROJECT_NAME?=my_project_name
 V_DOXY_ALL=$(filter DOXY_%,$(.VARIABLES))
 V_DOXY_ALL_SUFFIXES=$(V_DOXY_ALL:V_DOXY_%=%)
 
-doc: $(doc_file) .phony_explicit
+doc: $(doc_sentinel) .phony_explicit
+
+
+$(doc_sentinel): $(doc_file) $(all_out_files)
 	$(doxygen) $(doc_file)
+	@touch $@
 
 $(doc_file):
 	@$(doxymake) $(doc_file) $(hpp_files)
